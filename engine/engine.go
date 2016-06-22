d6c2200a237b16ed23946a3cbcd6d8ca0fdfd9aa
Huy Tran <kingbazoka@gmail.com>
2016-06-21 22:48:45 -0700

    :star2: yeah, it's worked

--- engine/engine.go
+++ engine/engine.go
@@ -1,44 +1,48 @@
 package engine
 
 import (
-  "log"
-  "io/ioutil"
-  "os"
-  "os/exec"
-  "path/filepath"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
 )
 
 type Engine struct {
 }
 
 func (e *Engine) Gen(input string) string {
-  return "package main\n\nfunc main() {\n" + input + "\n}"
+	return "package main\n\nfunc main() {\n" + input + "\n}"
 }
 
 func (e *Engine) Save(code string) (string, string) {
-  dir, err := ioutil.TempDir("", "go-play-engine-temp")
-  if err != nil {
-    log.Fatal(err)
-  }
-  tmpfn := filepath.Join(dir, "main.go")
-  if err := ioutil.WriteFile(tmpfn, []byte(code), 0666); err != nil {
-    log.Fatal(err)
-  }
-  return dir, tmpfn
+	dir, err := ioutil.TempDir("", "go-play-engine-temp")
+	if err != nil {
+		log.Fatal(err)
+	}
+	tmpfn := filepath.Join(dir, "main.go")
+	if err := ioutil.WriteFile(tmpfn, []byte(code), 0666); err != nil {
+		log.Fatal(err)
+	}
+	return dir, tmpfn
 }
 
 func (e *Engine) CleanUp(dir string) {
-  os.RemoveAll(dir)
+	os.RemoveAll(dir)
 }
 
 func (e *Engine) Run(code string) {
-  path, file := e.Save(e.Gen(code))
-  defer e.CleanUp(path)
+	path, _ := e.Save(e.Gen(code))
+	//defer e.CleanUp(path)
 
-  print("Executing... \n")
+	print("Executing... \n")
 
-  if err := exec.Command("go run " + file).Run(); err != nil {
-    log.Fatal(err)
-  }
+	os.Chdir(path)
+
+	cmdName := "go"
+	cmdArgs := []string{"run", "main.go"}
+	cmd := exec.Command(cmdName, cmdArgs...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
 }
-
--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import "github.com/huytd/go-play/engine"
 
 func main() {
-  e := engine.Engine{}
-  e.Run(`print("Hello")`)
+	e := engine.Engine{}
+	e.Run(`print("Hello World")
+  a := 1
+  b := 2
+  print("Result: ", a + b)`)
 }
