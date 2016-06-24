020028112d438ccfac5323814ec9638bbf036c8a
Huy Tran <kingbazoka@gmail.com>
2016-06-24 19:05:13 +0100

    :bug: enter key with ctrl on windows has keyCode = 10, wtf...

--- www/main.js
+++ www/main.js
@@ -6,7 +6,8 @@
 
 	$("#code").on("keypress", function(e) {
 		var meta = e.metaKey || e.ctrlKey;
-		var enter = (e.keyCode === 13) || (e.which === 13);
+		var enterKey = (e.keyCode || e.which);
+		var enter = enterKey === 10 || enterKey === 13;
 		if (meta && enter) {
 			$("#output").html("");
 			$("#output").append("<p class='ide'>Executing...</p>");
