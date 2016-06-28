8d274b2236450ba38907a119b5c06ce83e518f5e
Pawel Kowalak <pawel.kowalak@gmail.com>
2016-06-28 15:38:44 +0200

    Store default port number in const
    Also do a small refactor of web package.

--- web/server.go
+++ web/server.go
@@ -2,6 +2,7 @@ package web
 
 import (
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"path"
@@ -14,7 +15,6 @@ import (
 func runCode(w http.ResponseWriter, r *http.Request) {
 	e := engine.Engine{}
 	output, err := e.Run(r.FormValue("code"))
-
 	if err != nil {
 		http.Error(w, output, 400)
 		return
@@ -23,17 +23,27 @@ func runCode(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, string(output))
 }
 
-func Start() {
-	port := ":" + os.Getenv("PORT")
-	if port == ":" {
-		port = ":3000"
-	}
+const defaultPort = "3000"
 
-	println("Web IDE started at http://localhost", port)
+// Start launches an HTTP server and attaches handlers.
+func Start() {
+	port := envString("PLAYGO_PORT", defaultPort)
+
+	log.Printf("Starting web IDE at http://localhost:%s", port)
 
 	_, filename, _, _ := runtime.Caller(1)
 	fs := http.FileServer(http.Dir(path.Join(filepath.Dir(filename), "www")))
 	http.Handle("/", fs)
 	http.HandleFunc("/api/run", runCode)
-	http.ListenAndServe(port, nil)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Printf("Can't start HTTP listener: %v", err)
+	}
+}
+
+func envString(env, fallback string) string {
+	e := os.Getenv(env)
+	if e == "" {
+		return fallback
+	}
+	return e
 }
